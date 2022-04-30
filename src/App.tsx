7874f5bf2ac001594bfb24f58462ddbf741de504
Viacheslav Mudryk <vm3192@icloud.com>
2022-04-30 17:02:49 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const data = {"First name": "Slavik",
+	"Short biography": "I'm 30 years old. I wanna to learn React :)",
+	"Public Contacts": "telegram - @vm3192"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<p>{data['First name']}</p>
+			<p>{data['Short biography']}</p>
+			<p>{data['Public Contacts']}</p>
     </div>
   );
 }
